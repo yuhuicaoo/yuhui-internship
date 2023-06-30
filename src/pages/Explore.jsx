2c322b5bcde3fc9b3fef343918ae4ea4f4edd010
Yuhui <yuhuicaowork@gmail.com>
2023-06-30 17:38:13 +1200

    merge practice

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -31,7 +31,7 @@ const Explore = () => {
             </div>
           </div>
         </section>
-
+        {/* Merge Practice  */}
         <section aria-label="section">
           <div className="container">
             <div className="row" data-aos="fade" data-aos-duration="500">
